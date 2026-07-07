0b9ea5005dd011f49ca7f40b26b89330b8af69ac
Josh-Echendu <echendujosh@gmail.com>
2026-07-07 16:57:33 +0100

    initial-commit

--- WHATSAPP_BOT_API/core/config.py
+++ WHATSAPP_BOT_API/core/config.py
@@ -1,12 +1,25 @@
 from COMMON.config import *
 from COMMON.sessions import get_user_session, save_user_session
-from COMMON.redis import get_arq_redis
+from COMMON.redis import get_arq_redis, redis_client
+
+
+
 
 import httpx
 import asyncio
 import json
 import logging
 from decimal import Decimal
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)-8s | %(name)s | %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+logger = logging.getLogger("WHATSAPP_BOT")
+
 
 
 
@@ -14,3 +27,4 @@ load_dotenv(Path(__file__).resolve().parent.parent / ".env")
 APP_SECRET = os.getenv("META_APP_SECRET")
 VERIFY_TOKEN = os.getenv("META_VERIFY_TOKEN")
 PHONE_ID = os.getenv("WHATSAPP_PHONE_ID")
+
