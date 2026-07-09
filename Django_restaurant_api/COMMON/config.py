a4aaa0f6302762afee7c7090e9cb54e3c831d943
Josh-Echendu <echendujosh@gmail.com>
2026-07-09 11:33:44 +0100

    initial-commit

---
+++ Django_restaurant_api/COMMON/config.py
@@ -0,0 +1,2 @@
+from COMMON.logger_config import logger
+from COMMON.redis import redis_client
---
+++ FACEBOOK_BOT_API/entrypoint_facebook.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Ensure Redis/other services are reachable first (optional)
+sleep 5
+
+
+# Start the bot
+exec uvicorn webhook.webhook_server:app \
+    --host 0.0.0.0 \
+    --port 5000 \
+    --reload \
+    --log-level info
