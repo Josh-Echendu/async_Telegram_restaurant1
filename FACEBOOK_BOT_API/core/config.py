02bbd473903a6f9ba48ba2ee2180daa9412d8b55
Josh-Echendu <echendujosh@gmail.com>
2026-07-10 22:23:29 +0100

    initial-commit

--- FACEBOOK_BOT_API/core/config.py
+++ FACEBOOK_BOT_API/core/config.py
@@ -9,4 +9,8 @@ from decimal import Decimal
 
 
 
+
 load_dotenv(Path(__file__).resolve().parent.parent / ".env")
+APP_SECRET = os.getenv("META_APP_SECRET")
+VERIFY_TOKEN = os.getenv("META_VERIFY_TOKEN")
+
