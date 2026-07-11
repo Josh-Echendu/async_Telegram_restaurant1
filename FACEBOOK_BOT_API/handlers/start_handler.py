b60d0ac7e54329225ede1c6a2d0f720e08e0e51e
Josh-Echendu <echendujosh@gmail.com>
2026-07-11 14:27:44 +0100

    initial-commit

--- FACEBOOK_BOT_API/handlers/start_handler.py
+++ FACEBOOK_BOT_API/handlers/start_handler.py
@@ -232,38 +232,6 @@ async def start(event, restaurant_data):
 
 
 
-async def send_button_message(recipient_id, text, access_token, buttons=None):
-
-    payload = {
-        "recipient": {
-            "id": recipient_id,
-        },
-        "messaging_type": "RESPONSE",
-        "message": {
-            "attachment": {
-                "type": "template",
-                "payload": {
-                    "template_type": "button",
-                    "text": text,
-                    "buttons": buttons or [],
-                },
-            }
-        },
-    }
-
-    response, success = await _request_with_retry(
-        method="POST",
-        url="https://graph.facebook.com/v23.0/me/messages",
-        params={
-            "access_token": access_token,
-        },
-        json=payload,
-    )
-
-    return success
-
-
-
 
 
 
